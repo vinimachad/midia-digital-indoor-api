fb6eda1b8b572c79fad6353811b71db9aea5fe4b
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2024-01-28 00:00:54 -0400

    [REMOVED] Build folder and updated .gitignore with build folder

--- build/middlewares/errors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function errorHandler(error, req, res, next) {
-    console.log(error);
-    res.status(500).send({ errors: [{ message: 'Something went wrong' }] });
-}
-exports.default = errorHandler;
