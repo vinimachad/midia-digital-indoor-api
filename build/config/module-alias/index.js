216751ea9faf25745fff18f56ca90d9b94343de7
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2024-01-28 00:00:05 -0400

    [REMOVED] Build folder

--- build/config/module-alias/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const module_alias_1 = __importDefault(require("module-alias"));
-const aliases_1 = __importDefault(require("./aliases"));
-module_alias_1.default.addAliases(aliases_1.default);
