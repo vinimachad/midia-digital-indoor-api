f9d94b1c515dbc971c45e1789c282f9ceeaace16
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2024-02-01 14:28:21 -0400

    [UPDATE] Resolved PR comments

--- src/controllers/user/user-controller.ts
+++ src/controllers/user/user-controller.ts
@@ -2,10 +2,14 @@ import { userSchema } from '@models/zod/schemas/user'
 import CreateUserUseCase, {
   ICreateUserUseCase
 } from '@usecases/user/create-user-use-case'
+import CanRefreshTokenUseCaseUseCase, {
+  ICanRefreshTokenUseCaseUseCase
+} from '@usecases/user/refresh-token/can-refresh-token-use-case-use-case'
 import { Request, Response } from 'express'
 
 export default function UserController(
-  createUserUseCase: ICreateUserUseCase = new CreateUserUseCase()
+  createUserUseCase: ICreateUserUseCase = new CreateUserUseCase(),
+  canRefreshTokenUseCaseUseCase: ICanRefreshTokenUseCaseUseCase = new CanRefreshTokenUseCaseUseCase()
 ) {
   async function create(req: Request, res: Response) {
     let user = userSchema.parse(req).body
@@ -13,5 +17,12 @@ export default function UserController(
     res.status(200).json({ results })
   }
 
-  return { create }
+  async function refreshToken(req: Request, res: Response) {
+    let auth = req.headers.authorization ?? ''
+    let [_, token] = auth.split(' ')
+    let results = await canRefreshTokenUseCaseUseCase.execute(token)
+    res.status(200).json({ results })
+  }
+
+  return { create, refreshToken }
 }
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,32 +1,12 @@
-import jwt from '@configs/jwt'
 import UserController from '@controllers/user/user-controller'
-import useAuthentication from '@middlewares/auth/use-authentication'
 import useValidate from '@middlewares/zod/use-validate'
 import { userSchema } from '@models/zod/schemas/user'
-import CanRefreshTokenUseCaseUseCase from '@usecases/user/refresh-token/can-refresh-token-use-case-use-case'
 import { Router } from 'express'
 
 const userRoutes = Router()
 const userController = UserController()
 
 userRoutes.post('/register', useValidate(userSchema), userController.create)
-
-userRoutes.get('/jwt', (req, res) => {
-  res.status(200).json({
-    jwt: jwt().jwtToken().sign('7c807ff8-e66a-44a0-a322-d1eeeaf534bf')
-  })
-})
-
-userRoutes.get('/refresh-token', async (req, res) => {
-  let auth = req.headers.authorization ?? ''
-  let [_, token] = auth.split(' ')
-  let canRefreshToken = new CanRefreshTokenUseCaseUseCase()
-  let results = await canRefreshToken.execute(token)
-  res.status(200).json({ results })
-})
-
-userRoutes.get('/test', useAuthentication(), (req, res) => {
-  res.status(200).json({ message: 'ok' })
-})
+userRoutes.get('/refresh-token', userController.refreshToken)
 
 export default userRoutes
